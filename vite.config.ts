c8f36bc9344f27d0d1cd75a2cb10329aad8eed8e
Rhilip <admin@rhilip.info>
2022-02-04 20:56:57 +0800

    public

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import { defineConfig } from "vite";
+import browserExtension from "vite-plugin-web-extension";
+
+function root(...paths: string[]): string {
+  return path.resolve(__dirname, ...paths);
+}
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: root("dist"),
+    emptyOutDir: true,
+  },
+  plugins: [
+    browserExtension({
+      manifest: root("src/manifest.json"),
+      assets: "assets",
+      watchFilePaths: [root("src/manifest.json")],
+      verbose: true,
+      browser: process.env.TARGET || "chrome",
+      disableAutoLaunch: true
+    }),
+  ],
+});
